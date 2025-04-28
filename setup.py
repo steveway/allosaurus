569e40c6c0eebdabafd9259169f1ea7f37fbd70d
Stefan Murawski <steveway1@googlemail.com>
2025-04-28 10:06:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup,find_packages
 
 setup(
    name='allosaurus',
-   version='1.0.2',
+   version='1.0.3',
    description='a multilingual phone recognizer',
    author='Xinjian Li',
    author_email='xinjianl@cs.cmu.edu',
-   url="https://github.com/xinjli/allosaurus",
+   url="https://github.com/steveway/allosaurus",
    packages=find_packages(),
    install_requires=[
       'scipy',
       'numpy',
-      'resampy',
+      'soxr',
       'panphon',
       'torch',
       'editdistance',
